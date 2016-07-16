b1061896d174153fa375c9c4fee204f6c3dd7d2e
David Bürgin <676c7473@gmail.com>
2016-07-16 19:33:57 +0200

    More random acts of settling in

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,14 +20,34 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+;; Frequently used typographical marks.
+;; TODO Perhaps use typo-mode in the future?
+(global-set-key (kbd "<f5>") (kbd "‚"))
+(global-set-key (kbd "S-<f5>") (kbd "„"))
+(global-set-key (kbd "<f6>") (kbd "‘"))
+(global-set-key (kbd "S-<f6>") (kbd "“"))
+(global-set-key (kbd "<f7>") (kbd "’"))
+(global-set-key (kbd "S-<f7>") (kbd "”"))
+(global-set-key (kbd "<f8>") (kbd "…"))
+(global-set-key (kbd "<f9>") (kbd "–"))
+;; ... more generally, C-x 8 RET, then type Unicode name.
+
+;; Need this on OS X only
+(require 'exec-path-from-shell)
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
 
 (require 'org)
 (setq org-log-done t)
+(setq org-directory "~/org")
+(setq org-default-notes-file (concat org-directory "/notes.org"))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-; or instead, (define-key global-map "\C-ca" 'org-agenda), etc.?
+;; or instead, (define-key global-map "\C-ca" 'org-agenda), etc.?
 
 (require 'ido)
 (ido-mode t)
