9eb55045251c25417e91ff095dc2f32d4e049aad
David Bürgin <676c7473@gmail.com>
2016-07-16 09:29:58 +0200

    Enable Org mode
    Also, don't double space.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -9,9 +9,19 @@
       `(("." . ,(expand-file-name
                  (concat user-emacs-directory "backups")))))
 
+(setq sentence-end-double-space nil)
+
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(require 'org)
+(setq org-log-done t)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+; or instead, (define-key global-map "\C-ca" 'org-agenda), etc.?
 
 (require 'ido)
 (ido-mode t)
